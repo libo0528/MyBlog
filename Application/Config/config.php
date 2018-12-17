6e5385716882777f42b4319a5be469d9e73736b8
libo0528 <45119976+libo0528@users.noreply.github.com>
2018-12-17 11:08:52 +0800

    Add files via upload

--- Application/Config/config.php
+++ Application/Config/config.php
@@ -1,1 +1,25 @@
+<?php
+return array(
+    //数据库配置信息
+    'database'=>array(
+        'type'=>'mysql',
+        'user'=>'root',
+        'psw'=>'root',
+        'dbname'=>'mvc'
+        
+    ),
+    //应用程序配置信息
+    'app'=>array(
+        'upload_path'   =>'./Public/Uploads/',
+        'upload_size'   =>2222222,
+        'upload_type'   =>array('image/png','image/gif','image/jpeg','image/jpg'),
+        'default_platform'=>'Home',
+        'default_controller'=>'Index',
+        'default_action'=>'index',
+        //用来记录开发模式还是运行模式
+        'debug'         =>true,
+        'pagesize'      =>2
+        
+    )
+);
 
